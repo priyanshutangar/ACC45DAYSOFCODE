e26c0b0ad3ea4b336bf24d72165104265f5cf3af
priyanshutangar <116300269+priyanshutangar@users.noreply.github.com>
2022-11-04 22:37:54 +0530

    Create Day15.cpp

---
+++ Day15.cpp
@@ -0,0 +1,15 @@
+//max dif min
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	for(int i=0;i<a;i++)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<c-a<<endl;
+	}
+	return 0;
+}
