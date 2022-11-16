054d199916c7e4e2d037870a4668506e4dd2ca58
priyanshutangar <116300269+priyanshutangar@users.noreply.github.com>
2022-11-16 22:10:21 +0530

    Create Day27.cpp

---
+++ Day27.cpp
@@ -0,0 +1,22 @@
+//reach the target
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	for(int i=0; i<a; i++)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y)
+	    {
+	        cout<<x-y<<endl;
+	    }
+	    else
+	    {
+	        cout<<y-x<<endl;
+	    }
+	}
+	return 0;
+}
