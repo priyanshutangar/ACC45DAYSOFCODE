2b6cfb1c40b8a42c6342217d97d7380b6286aea4
priyanshutangar <116300269+priyanshutangar@users.noreply.github.com>
2022-11-18 20:40:41 +0530

    Create Day29.cpp

---
+++ Day29.cpp
@@ -0,0 +1,22 @@
+//police and theif 
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	for(int i=0;i<a;i++)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y)
+	    {
+	        cout<<x-y<<endl;
+	    }
+	    else
+	    {
+	        cout<<y-x<<endl;
+	    }
+	}
+	return 0;
+}
