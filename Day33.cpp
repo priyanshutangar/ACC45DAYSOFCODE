c6790191d6700d45a78c815e00ca87e662686fb0
priyanshutangar <116300269+priyanshutangar@users.noreply.github.com>
2022-11-22 21:59:08 +0530

    Create Day33.cpp

---
+++ Day33.cpp
@@ -0,0 +1,15 @@
+//fitness
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    for(int i; i<a; i++)
+    {
+        int x;
+        cin>>x;
+        cout<<x*2<<endl;
+    }
+	return 0;
+}
