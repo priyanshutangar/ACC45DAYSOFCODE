f88565070a52bc6589f9a6e22ac41b311d853026
priyanshutangar <116300269+priyanshutangar@users.noreply.github.com>
2022-12-03 19:50:10 +0530

    Create Day44.cpp

---
+++ Day44.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	for(int i=0;i<a;i++)
+	{
+	    int x;
+	    cin>>x;
+	    if(x>98)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
